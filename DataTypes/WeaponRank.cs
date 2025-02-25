c4957f7c70ddc7f556fcf7ddf010fed59e3bde0f
Kyle S <kstephenson1@my.wctc.edu>
2025-02-25 02:11:10 -0600

    Added weapon enums

---
+++ DataTypes/WeaponRank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace w5_assignment_ksteph.DataTypes
+{
+    [JsonConverter(typeof(JsonStringEnumConverter<WeaponRank>))]
+    public enum WeaponRank
+    {
+        [EnumMember(Value = "E")]
+        E = 0,
+        [EnumMember(Value = "D")]
+        D = 1,
+        [EnumMember(Value = "C")]
+        C = 2,
+        [EnumMember(Value = "B")]
+        B = 3,
+        [EnumMember(Value = "A")]
+        A = 4,
+        [EnumMember(Value = "S")]
+        S = 5,
+    }
+}
---
+++ DataTypes/WeaponType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace w5_assignment_ksteph.DataTypes
+{
+    [JsonConverter(typeof(JsonNumberEnumConverter<WeaponType>))]
+    public enum WeaponType
+    {
+        None = 0,
+        Sword = 1,
+        Axe = 2,
+        Lance = 3,
+        Bow = 4,
+        Elemental = 5,
+        Dark = 6,
+        Light = 7,
+        Heal = 8
+    }
+}
